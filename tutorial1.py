0ee359382a01d3d3dafe7eef70767a0e3ee4d0ee
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-02-01 16:38:16 -0500

    Updated from Brython Server: 2/1/2019 4:38:13 PM

--- tutorial1.py
+++ tutorial1.py
@@ -1,17 +1,18 @@
 from ggame import App, Color, LineStyle, Sprite
-from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
-#3 primary colors with no transparency (alpha=1.0)
-red = Color(0xff000, 1.0)
+from ggame import CircleAsset
+red = Color(0xff0000, 1.0)
 green= Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-# Define a line style that is a thin (1 pixel) wide black line
-thinline = LineStyle(1, black)
-# A graphics asset that represents a rectangle
-rectangle = RectangleAsset(50, 20, thinline, blue)
 
-# Now display a rectangle
-Sprite(rectangle, (200,50))
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+other= CircleAsset (5,thinline,red)
+xcoordinates = range(100, 600, 10)
+
+# Generate a list of sprites that form a line!
+sprite2 = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+sprite1=[Sprite(other, (x,(-0.5)*x+200)) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
